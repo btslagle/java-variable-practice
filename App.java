e52d400a4850222bb4c0ad87c085da2d53216ce7
Brad Slagle <btslagle@gmail.com>
2022-05-24 12:13:26 -0500

    Update answers

--- App.java
+++ App.java
@@ -1,6 +1,23 @@
 class App {
   public static void main(String args[]){
     String message = "Hello, world!";
+    int vehicleID = 1000001;
+    String vehicleColor = "Red";
+    boolean towPackage = true;
+    double  odometerReading = 32_000.15;
+    double price = 32.01;
+    char qualityRating = 'A';
+
     System.out.println(message);
+    System.out.println(vehicleID);
+    System.out.println(vehicleColor);
+    System.out.println(towPackage);
+    System.out.println(odometerReading);
+    System.out.println(price);
+    System.out.println(qualityRating);
+    
+    
+  
+
   }
 }
